dad871903c6de1fcfd5f02b84a1e121bdb65c1cc
benex drake <benex@hotmail.de>
2025-02-09 21:46:36 +0100

    cleaning

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import LoginOutButton from "@/components/login_btn";
-import { addProject, deleteProject, getProjectById, updateProject } from "@/services/project_services";
-import { addRequest, deleteRequest, getRequestById, getRequests, updateRequest } from "@/services/request_services";
-import { addUser, deleteUser, getUserById, getUsers, updateUser } from "@/services/user_services";
 import axios from "axios";
 
 export default function Home() 
@@ -16,65 +13,6 @@ export default function Home()
 
 export async function getServerSideProps(context:any)
 {
-  // const user = {
-  //   id: "1",
-  //   username: "string111",
-  //   avatar: "string",
-  //   accountType: "string",
-  //   banner: "string",
-  //   discordUrl: "string",
-  //   xUrl: "string",
-  //   websiteUrl: "string",
-  //   email: "string",
-  //   projectId: "string"
-  // }
-  
-
-  // await axios.get('http://localhost:3000/api/user?key=77a8547f-707e-445c-96a0-c1066f7496e4');
-
-  // console.log('GET USERS:',await getUsers());
-  // console.log('GET USER:',await getUserById('string'));
-  // console.log('POST USER:',await addUser(user));
-  // console.log('PUT USER:',await updateUser(user));
-  // console.log('DELETE USER:',await deleteUser('1'));
-
-  // const project = {
-  //   id: "1",
-  //   header: "string",
-  //   title: "string",
-  //   description: "string",
-  //   userIds: [
-  //     "string"
-  //   ],
-  //   elementIds: [
-  //     "string"
-  //   ]
-  // }
-
-  // console.log('POST PROJECT', await addProject(project));
-  // console.log('GET PROJECT:', await getProjectById('1'));
-  // console.log('PUT PROJECT', await updateProject(project));
-  // console.log('DELETE PROJECT', await deleteProject('1'));
-
-  // const request = {
-  //   id: "string",
-  //   title: "string",
-  //   description: "string",
-  //   fileUrl: "string",
-  //   created: "2025-02-09T15:12:21.791Z",
-  //   projectId: "string",
-  //   userId: "string"
-  // }
-
-  // console.log('GET REQUESTS:', await getRequests());
-  // console.log('POST REQUEST:', await addRequest(request));
-  // console.log('GET REQUEST:', await getRequestById('string'));
-  // console.log('PUT REQUEST:', await updateRequest(request));
-  // console.log('DELETE REQUEST:', await deleteRequest('string'));
-  
-     
-
-
     return {
         props: {
 
--- services/discord_service.ts
+++ services/discord_service.ts
@@ -9,10 +9,7 @@ export async function getDiscordUser(token:string)
     };
     const d = await axios.get('https://discord.com/api/users/@me', config).then(x => { return x.data})
 
-    const user:User = {id:d.id, username:d.global_name, avatar:`https://cdn.discordapp.com/avatars/${d.id}/${d.avatar}`, accountType:'discord', banner:d.banner, discordUrl:'', xUrl:'', websiteUrl:'', email:'', projectId:''}
-
-    console.log(user);
+    const user:User = {id:d.id, username:d.global_name, avatar:`https://cdn.discordapp.com/avatars/${d.id}/${d.avatar}`, accountType:'discord', banner:"", discordUrl:'', xUrl:'', websiteUrl:'', email:'', projectId:''}
     
-
     await addUser(user);
 }
