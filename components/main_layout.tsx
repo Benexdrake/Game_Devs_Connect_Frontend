324416ae45c5af1e0fbc6d7c2d01ea5c82f31605
benex <benex@hotmail.de>
2025-02-13 17:42:31 +0100

    adding ligin button for debugging

--- components/main_layout.tsx
+++ components/main_layout.tsx
@@ -3,6 +3,7 @@ import Navbar from './nav/navbar'
 import CategoryBar from './nav/categorybar'
 import Footer from './nav/footer'
 import { useSession } from 'next-auth/react'
+import LoginOutButton from './login_btn'
 
 export default function MainLayout(props:any)
 {
@@ -15,7 +16,7 @@ export default function MainLayout(props:any)
                 <Navbar/>
             </nav>
             <main className={styles.child}>
-                { session ? ( <div>{children}</div> ) : ( <p>Please Log in...</p> )}
+                { session ? ( <div>{children}</div> ) : ( <LoginOutButton/> )}
             </main>
             <nav className={styles.sidebar}>
                 <CategoryBar/>
