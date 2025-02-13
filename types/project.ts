34197c5901fa0ec0dfc52775048e9b36aabd194b
benex <benex@hotmail.de>
2025-02-13 11:15:41 +0100

    adding types

--- types/project.ts
+++ types/project.ts
@@ -1,4 +1,10 @@
 export type Project =
 {
-    
+    id:string;
+    header:string;
+    title:string;
+    description:string;
+    ownerId:string;
+    teamIds:string[];
+    elementIds:string[];
 }
--- types/request.ts
+++ types/request.ts
@@ -1,4 +1,10 @@
 export type Request =
 {
-    
+    id:string;
+    title:string;
+    description:string;
+    fileUrl:string;
+    created:string;
+    projectId:string;
+    userId:string;
 }
