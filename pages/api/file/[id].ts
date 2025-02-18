d4defe7c1a0c6569f5cebf538ed5dea554a6705d
benex <benex@hotmail.de>
2025-02-18 16:47:45 +0100

    get file from backend

--- pages/api/file/[id].ts
+++ pages/api/file/[id].ts
@@ -1,6 +1,16 @@
+import { getFileById } from "@/services/file_service";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> 
 {
+    const id = parseInt(req.query.id as string)
+
+    console.log(req.query);
+    
+    if(!id || id == 0) res.status(200).send(0)
+
+    const result = await getFileById(id);
+
+    res.status(200).json(result);
 
 }
