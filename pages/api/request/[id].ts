d3ff9915c3948c4406cd7b5e0073d7d96218b3b0
benex <benex@hotmail.de>
2025-02-18 19:40:40 +0100

    secure api with getServerSession

--- pages/api/request/[id].ts
+++ pages/api/request/[id].ts
@@ -1,11 +1,21 @@
 import { getRequestById } from "@/services/backend/request_services";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../auth/[...nextauth]";
 
 export default async function handler( req: NextApiRequest, res: NextApiResponse)
 {
     const id = req.query.id
+    const accessToken = req.query.accessToken
 
-    const result = await getRequestById(id as string)
+    const session = await getServerSession(req,res,authOptions);
+    
+    console.log('Session:',session);
 
-    res.status(200).json(result);
+    if(session || accessToken)
+    {
+        const result = await getRequestById(id as string)
+        res.status(200).json(result);
+    }
+    res.status(500).send('Go away!!!')
 }
