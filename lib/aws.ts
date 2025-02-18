7cb551a8a5c5d4d2021502441356c1fd5e77f946
benex <benex@hotmail.de>
2025-02-18 12:27:37 +0100

    adding lib for aws

---
+++ lib/aws.ts
@@ -0,0 +1,58 @@
+'use server'
+
+import { NextApiRequest } from "next";
+
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import { FileType } from "@/types/file";
+
+// Get File from S3 by ID
+export const getFileS3 = async () =>
+{
+
+}
+
+// Add File to S3
+export const addFileS3 = async (file:any, id:number) =>
+{
+    try {
+        const s3 = new AWS.S3();
+
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME as string,
+            Key: `${id}/${file.originalFilename}`,
+            Body: fs.createReadStream(file.filepath),
+          };
+
+        return await new Promise((resolve, reject) =>
+        {
+            s3.upload(params, (err:any,data:any) =>
+            {
+                if(err)
+                {
+                    console.error('Fehler beim Hochladen in S3:', err);
+                    resolve(false)
+                }
+                console.log('Datei erfolgreich in S3 hochgeladen:', data.Location);
+                resolve(true)
+            })
+        })
+    } 
+    catch (error: any) 
+    {
+        return false;
+    }
+}
+
+
+// Update File at S3
+export const updateFileS3 = async (file:FileType) =>
+{
+
+}
+
+// Delete File from S3
+export const deleteFileS3 = async (id:number) =>
+{
+
+}
